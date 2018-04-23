199fd6ec3b5b1c90c2372e791b6586d99160a450
AntonioGUJ <36006413+AntonioGUJ@users.noreply.github.com>
2018-04-23 22:07:49 +0200

    Add files via upload

--- KerasPrototypes/SlidingWindowBatchGenerator.py
+++ KerasPrototypes/SlidingWindowBatchGenerator.py
@@ -10,22 +10,25 @@
 
 from Preprocessing.SlidingWindowImages import *
 from keras.preprocessing import image
-from keras import backend as K
+from keras.backend import image_data_format as K_image_data_format
+from keras.utils import to_categorical as K_to_categorical
 import numpy as np
 np.random.seed(2017)
 
 
 class SlidingWindowBatchGenerator(image.Iterator):
 
-    def __init__(self, list_Xdata, list_Ydata, size_image, prop_overlap, size_outNnet=None, batch_size=1, shuffle=True, seed=None):
-
+    def __init__(self, list_Xdata, list_Ydata, size_image, prop_overlap, num_channels_in=1, num_classes_out=1, size_outnnet=None, batch_size=1, shuffle=True, seed=None):
         self.list_Xdata = list_Xdata
         self.list_Ydata = list_Ydata
-        self.size_image = size_image
-        if size_outNnet and (size_outNnet != size_image):
-            self.size_outNnet = size_outNnet
+
+        self.size_image      = size_image
+        self.num_channels_in = num_channels_in
+        self.num_classes_out = num_classes_out
+        if size_outnnet and (size_outnnet != size_image):
+            self.size_outnnet = size_outnnet
         else:
-            self.size_outNnet = size_image
+            self.size_outnnet = size_image
 
         self.type_Xdata = list_Xdata[0].dtype
         self.type_Ydata = list_Ydata[0].dtype
@@ -71,8 +74,8 @@ class SlidingWindowBatchGenerator(image.Iterator):
         # overwrite function to retrieve images batches
         num_images_batch = len(index_array)
 
-        Xdata_batch = np.ndarray([num_images_batch] + list(self.size_image  ), dtype=self.type_Xdata)
-        Ydata_batch = np.ndarray([num_images_batch] + list(self.size_outNnet), dtype=self.type_Ydata)
+        Xdata_batch = np.ndarray([num_images_batch] + list(self.size_image)  + [self.num_channels_in], dtype=self.type_Xdata)
+        Ydata_batch = np.ndarray([num_images_batch] + list(self.size_outnnet)+ [self.num_classes_out], dtype=self.type_Ydata)
 
         for i, index in enumerate(index_array):
 
@@ -81,35 +84,50 @@ class SlidingWindowBatchGenerator(image.Iterator):
             ((z_back, z_front), (x_left, x_right), (y_down, y_up)) = self.slidingWindowImages.get_limits_image_3d((idx_z_image, idx_x_image, idx_y_image))
 
             Xdata_batch[i] = self.list_Xdata[idx_fullimage][z_back:z_front, x_left:x_right, y_down:y_up]
-            Ydata_batch[i] = self.get_array_cropImages_outNnet(self.list_Ydata[idx_fullimage][z_back:z_front, x_left:x_right, y_down:y_up])
+
+            if self.num_classes_out > 1:
+                Ydata_batch[i] = self.get_array_categorical_masks(self.get_array_cropImages_outNnet(self.list_Ydata[idx_fullimage][z_back:z_front, x_left:x_right, y_down:y_up]))
+            else:
+                Ydata_batch[i] = self.get_array_cropImages_outNnet(self.list_Ydata[idx_fullimage][z_back:z_front, x_left:x_right, y_down:y_up])
         #endfor
 
         return (self.get_array_reshapedKeras(Xdata_batch), self.get_array_reshapedKeras(Ydata_batch))
 
 
+    def get_array_categorical_masks(self, yData):
+        return K_to_categorical(yData, num_classes=self.num_classes_out)
+
+
     @staticmethod
-    def getArrayShapeKeras(num_batches, size_image):
-        if K.image_data_format() == 'channels_first':
-            return [num_batches, 1] + list(size_image)
+    def getArrayShapeKeras(num_batches, size_image, num_channels):
+        if K_image_data_format() == 'channels_first':
+            return [num_batches, num_channels] + list(size_image)
         else:
-            return [num_batches] + list(size_image) + [1]
+            return [num_batches] + list(size_image) + [num_channels]
 
-    @classmethod
-    def get_array_reshapedKeras(cls, array):
-        return array.reshape(cls.getArrayShapeKeras(array.shape[0], array.shape[1:]))
+    def get_array_reshapedKeras(self, array):
+        if len(array.shape) == (len(self.size_image) + 1):
+            # array_shape: (num_batch, size_batch) #not multichannel
+            return array.reshape(self.getArrayShapeKeras(array.shape[0], array.shape[1:], 1))
+        else:
+            # array_shape: (num_batch, size_batch, num_channel) #multichannel
+            return array.reshape(self.getArrayShapeKeras(array.shape[0], array.shape[2:-1], array.shape[-1]))
+
 
     @staticmethod
-    def get_limits_CropImage(size_image, size_outNnet):
-        if (size_image==size_outNnet):
+    def get_limits_cropImage(size_image, size_outnnet):
+        if (size_image==size_outnnet):
             return size_image
         else:
-            return tuple(((s_i- s_o)/2, (s_i + s_o)/2) for (s_i, s_o) in zip(size_image, size_outNnet))
+            return tuple(((s_i- s_o)/2, (s_i + s_o)/2) for (s_i, s_o) in zip(size_image, size_outnnet))
 
     def get_array_cropImages_outNnet(self, yData):
-
-        if (self.size_image==self.size_outNnet):
+        if (self.size_image==self.size_outnnet):
             return yData
         else:
-            ((z_back, z_front), (x_left, x_right), (y_down, y_up)) = self.get_limits_CropImage(self.size_image, self.size_outNnet)
-            print ((z_back, z_front), (x_left, x_right), (y_down, y_up))
-            return yData[..., z_back:z_front, x_left:x_right, y_down:y_up]
+            if len(self.size_image)==2:
+                ((x_left, x_right), (y_down, y_up)) = self.get_limits_cropImage(self.size_image, self.size_outnnet)
+                return yData[..., x_left:x_right, y_down:y_up]
+            elif len(self.size_image)==3:
+                ((z_back, z_front), (x_left, x_right), (y_down, y_up)) = self.get_limits_cropImage(self.size_image, self.size_outnnet)
+                return yData[..., z_back:z_front, x_left:x_right, y_down:y_up]
