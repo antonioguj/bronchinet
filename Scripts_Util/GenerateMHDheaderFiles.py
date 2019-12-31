878dee4018129eca1214a8463e947842fa96e3e9
A. Garcia-Uceda Juarez <antonio.garciauceda89@gmail.com>
2019-12-31 12:02:22 +0100

    Commit 31-12-2019

---
+++ Scripts_Util/GenerateMHDheaderFiles.py
@@ -0,0 +1,68 @@
+#
+# created by
+# Antonio Garcia-Uceda Juarez
+# PhD student
+# Medical Informatics
+#
+# created on 09/02/2018
+# Last update: 09/02/2018
+########################################################################################
+
+from DataLoaders.FileReaders import *
+from Common.FunctionsUtil import *
+import numpy as np
+import argparse
+
+
+
+def main(args):
+    # ---------- SETTINGS ----------
+    InputPath = args.inputdir
+    OutputPath = args.outputdir
+    namesOutputFiles = lambda in_name: filenamenoextension(in_name) + '.mhd'
+    # ---------- SETTINGS ----------
+
+    listInputFiles = findFilesDirAndCheck(InputPath)
+    makedir(OutputPath)
+
+
+    for in_file in listInputFiles:
+        print("\nInput: \'%s\'..." % (basename(in_file)))
+
+        in_image_size = FileReader.getImageSize(in_file)
+        out_image_size = np.roll(in_image_size, 2)
+
+        ndims = len(out_image_size)
+        str_out_image_size = ' '.join([str(elem) for elem in out_image_size])
+        name_raw_file = filenamenoextension(in_file) +'.raw'
+
+
+        #contruct mhd header file
+        str_info  = 'ObjectType = Image\n'
+        str_info += 'NDims = %s\n' %(ndims)
+        str_info += 'BinaryData = True\n'
+        str_info += 'DimSize = %s\n' %(str_out_image_size)
+        str_info += 'ElementType = MET_UCHAR\n'
+        str_info += 'ElementDataFile = %s' %(name_raw_file)
+
+        out_file = joinpathnames(OutputPath, namesOutputFiles(in_file))
+        print("Output: \'%s\'..." % (basename(out_file)))
+
+        fout = open(out_file, 'w')
+        fout.write(str_info)
+        fout.close()
+    #endfor
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputdir', type=str)
+    parser.add_argument('outputdir', type=str)
+    args = parser.parse_args()
+
+    print("Print input arguments...")
+    for key, value in vars(args).iteritems():
+        print("\'%s\' = %s" %(key, value))
+
+    main(args)
