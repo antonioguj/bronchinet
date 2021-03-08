584953b175654b99fd54895dc55f6975d9f30e72
AntonioGUJ <antonio.garciauceda@gmail.com>
2021-03-08 15:55:13 +0100

    Changes script p_test

--- scripts_util/compare_ptest_two_data.py
+++ scripts_util/compare_ptest_two_data.py
@@ -13,9 +13,12 @@ def main(args):
     elif args.type_ptest == 'related':
         fun_ttest = ttest_rel
 
-    list_input_files = [args.inputfile_1, args.inputfile_2]
+    print('\nCompare the field \'%s\' between two data, using \'%s\' P-test...' % (args.in_fieldname, args.type_ptest))
 
-    list_two_data = []
+
+    list_input_files = [args.inputfile_1, args.inputfile_2]
+    list_input_data = []
+    list_calc_stats_data = []
 
     for input_file in list_input_files:
 
@@ -29,12 +32,21 @@ def main(args):
             catch_error_exception(message)
         else:
             index_infieldname = fields_names_this.index(args.in_fieldname) + 1
-            list_two_data.append(raw_data_this_float[1:, index_infieldname])
+            data_this = raw_data_this_float[1:, index_infieldname]
+            list_input_data.append(data_this)
+
+            median_data_this = np.median(data_this)
+            perc25_data_this = np.percentile(data_this, 25)
+            perc75_data_this = np.percentile(data_this, 75)
+            stats_data_this  = (median_data_this, perc25_data_this, perc75_data_this)
+            list_calc_stats_data.append(stats_data_this)
     # endfor
 
-    (_, ptest_two_data) = fun_ttest(list_two_data[0], list_two_data[1])
+    (pstats_diff, pvalue_diff) = fun_ttest(list_input_data[0], list_input_data[1])
 
-    print('\nComputed %s P-test between two data: %s...' %(args.type_ptest, ptest_two_data))
+    print('data 1: %.3f (%.3f - %.3f)...' %(list_calc_stats_data[0][:]))
+    print('data 2: %.3f (%.3f - %.3f)...' %(list_calc_stats_data[1][:]))
+    print('Computed P-value (d1 - d2): %.6f...' %(pvalue_diff))
 
 
 
@@ -47,7 +59,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.type_ptest not in LIST_TYPE_PTESTS_AVAIL:
-        message = 'Input param \'type_ptest\' = \'%s\' not valid, must be inside: \'%s\'...' %(args.type_ptest, LIST_TYPE_PTESTS_AVAIL)
+        message = 'Input param \'type_ptest\' = \'%s\' not valid, must be: \'%s\'...' %(args.type_ptest, LIST_TYPE_PTESTS_AVAIL)
         catch_error_exception(message)
 
     print("Print input arguments...")
