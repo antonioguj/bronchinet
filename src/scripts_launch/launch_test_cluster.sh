84331acdfc5b6ffbe793ed22c42df7bcc5d0fa04
antonioguj <antonio.garciauceda89@gmail.com>
2021-04-09 17:43:37 +0200

    change location launch test script

--- src/scripts_launch/launch_test_cluster.sh
+++ src/scripts_launch/launch_test_cluster.sh
@@ -4,8 +4,8 @@
 #SBATCH -p hm
 #SBATCH --gres=gpu:1
 #SBATCH -t 01:00:00
-#SBATCH -o ./Logfiles/out_%j.log
-#SBATCH -e ./Logfiles/error_%j.log
+#SBATCH -o ./Logs/out_%j.log
+#SBATCH -e ./Logs/error_%j.log
 
 # Load the modules
 module purge
