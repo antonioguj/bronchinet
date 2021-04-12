6cbb93aebfd71d2485f34b9c2a76691351fac778
antonioguj <antonio.garciauceda89@gmail.com>
2021-04-12 18:08:04 +0200

    improve script get_converged_epoch

--- src/scripts_util/get_converged_epoch_model.py
+++ src/scripts_util/get_converged_epoch_model.py
@@ -9,32 +9,32 @@ from common.exceptionmanager import catch_error_exception
 def main(args):
 
     # SETTINGS
-    if args.is_output_move_aver_loss and args.is_move_aver:
-        out_aver_losshist_filename = args.input_loss_file.replace('.csv', '') + '_move-aver-%d.csv' % (args.size_aver)
+    if args.is_output_aver_loss and args.is_move_aver:
+        out_aver_losshist_filename = args.input_loss_file.replace('.csv', '') + '_aver-%d.csv' % (args.size_aver)
     else:
         out_aver_losshist_filename = None
     # --------
 
     epochs = None
     data_fields = None
-    index_field_eval_conver = None
-    data_eval_conver_original = None
+    index_field_eval = None
+    data_eval_original = None
 
     with open(args.input_loss_file, 'r') as infile:
         header_line = infile.readline()
         header_items = [elem.replace('/', '').replace('\n', '') for elem in header_line.split(' ')]
 
-        if args.field_eval_conver not in header_items:
+        if args.field_eval not in header_items:
             message = 'field chosen to evaluate the convergence of losses (\'%s\') not found in loss file' \
-                      % (args.field_eval_conver)
+                      % (args.field_eval)
             catch_error_exception(message)
         else:
-            index_field_eval_conver = header_items.index(args.field_eval_conver) - 1
+            index_field_eval = header_items.index(args.field_eval) - 1
 
         data_loss_file = np.loadtxt(args.input_loss_file, skiprows=1, delimiter=' ')
         epochs = data_loss_file[:, 0].astype(np.uint16)
         data_fields = data_loss_file[:, 1:]
-        data_eval_conver_original = data_fields[:, index_field_eval_conver]
+        data_eval_original = data_fields[:, index_field_eval]
 
     if args.is_move_aver:
         print('Compute the moving average of the losses, with size of average window \'%s\'...' % (args.size_aver))
@@ -52,7 +52,7 @@ def main(args):
 
         data_fields = aver_data_fields
 
-    if args.is_output_move_aver_loss and args.is_move_aver:
+    if args.is_output_aver_loss and args.is_move_aver:
         print("Write out the computed moving average of the losses in file: %s...\n" % (out_aver_losshist_filename))
         fout = open(out_aver_losshist_filename, 'w')
 
@@ -70,24 +70,24 @@ def main(args):
 
     # ******************************
 
-    data_eval_conver = data_fields[:, index_field_eval_conver]
+    data_eval = data_fields[:, index_field_eval]
 
     print('Evaluate the relative difference of the \'%s\' history, between epochs with patience \'%s\'...'
-          % (args.field_eval_conver, args.patience_eval_conver))
+          % (args.field_eval, args.patience_converge))
     print('Thresholds to mark convergence: %s, and divergence: %s...' % (args.thres_converge, args.thres_diverge))
     is_found_converge = False
     is_found_diverge = False
     epoch_converged = None
     epoch_diverged = None
 
-    list_epochs_evaluate = list(epochs[args.patience_eval_conver:])
+    list_epochs_evaluate = list(epochs[args.patience_converge:])
 
     for i_epoch_eval in list_epochs_evaluate:
         i_epoch_original = i_epoch_eval + args.size_aver - 1
 
-        value_eval_this = data_eval_conver[i_epoch_eval - 1]
-        value_eval_compare = data_eval_conver[i_epoch_eval - args.patience_eval_conver - 1]
-        # value_original = data_eval_conver_original[i_epoch_original - 1]
+        value_eval_this = data_eval[i_epoch_eval - 1]
+        value_eval_compare = data_eval[i_epoch_eval - args.patience_converge - 1]
+        # value_original = data_eval_original[i_epoch_original - 1]
 
         relerror_diff_values = abs((value_eval_this - value_eval_compare) / (value_eval_this + 1.0e-12))
 
@@ -98,9 +98,9 @@ def main(args):
             if relerror_diff_values < args.thres_converge:
                 is_found_converge = True
                 epoch_converged = i_epoch_original
-                epoch_min_value = 1 + np.argmin(data_eval_conver_original[:epoch_converged])
-                value_converged = data_eval_conver_original[epoch_converged - 1]
-                value_min_found = data_eval_conver_original[epoch_min_value - 1]
+                epoch_min_value = 1 + np.argmin(data_eval_original[:epoch_converged])
+                value_converged = data_eval_original[epoch_converged - 1]
+                value_min_found = data_eval_original[epoch_min_value - 1]
 
                 print("CONVERGED: at epoch \'%s\' with value \'%s\' and relative error \'%0.6f\'"
                       ". Min value found until here: \'%s\', at epoch \'%s\'...\n"
@@ -110,28 +110,28 @@ def main(args):
             if relerror_diff_values > args.thres_diverge:
                 is_found_diverge = False
                 epoch_diverged = i_epoch_original
-                value_diverged = data_eval_conver_original[epoch_diverged - 1]
+                value_diverged = data_eval_original[epoch_diverged - 1]
                 print("DIVERGED: at epoch \'%s\' with value \'%s\' and relative error \'%0.6f\'...\n"
                       % (epoch_diverged, value_diverged, relerror_diff_values))
                 # break
     # endfor
 
     if is_found_converge:
-        print("\nGOOD: HISTORY OF \'%s\' IS CONVERGED AT EPOCH \'%s\'..." % (args.field_eval_conver, epoch_converged))
+        print("\nGOOD: HISTORY OF \'%s\' IS CONVERGED AT EPOCH \'%s\'..." % (args.field_eval, epoch_converged))
     elif is_found_diverge:
-        print("\nBAD: HISTORY OF \'%s\' IS DIVERGED AT EPOCH \'%s\'..." % (args.field_eval_conver, epoch_diverged))
+        print("\nBAD: HISTORY OF \'%s\' IS DIVERGED AT EPOCH \'%s\'..." % (args.field_eval, epoch_diverged))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_loss_file', type=str)
-    parser.add_argument('--field_eval_conver', type=str, default='val_loss')
-    parser.add_argument('--patience_eval_conver', type=int, default=20)
+    parser.add_argument('--field_eval', type=str, default='val_loss')
+    parser.add_argument('--patience_converge', type=int, default=20)
     parser.add_argument('--thres_converge', type=float, default=0.001)
     parser.add_argument('--thres_diverge', type=float, default=0.05)
     parser.add_argument('--is_move_aver', type=bool, default=True)
     parser.add_argument('--size_aver', type=int, default=50)
-    parser.add_argument('--is_output_move_aver_loss', type=bool, default=False)
+    parser.add_argument('--is_output_aver_loss', type=bool, default=False)
     args = parser.parse_args()
 
     print("Print input arguments...")
