fbcd3dfd88a636b19678cb988bba48ffe2659361
antonioguj <antonio.garciauceda89@gmail.com>
2021-05-15 18:24:33 +0200

    implement cuboid in create homemade mask

--- src/scripts_util/create_homemade_mask.py
+++ src/scripts_util/create_homemade_mask.py
@@ -5,6 +5,8 @@ import argparse
 
 from dataloaders.imagefilereader import ImageFileReader
 
+_EPS = 1.0e-10
+
 
 def get_indexes_canditate_inside_shape(point_center: Tuple[float, float, float],
                                        max_dist_2cen: float
@@ -33,17 +35,20 @@ def create_sphere_mask(inout_image: np.ndarray,
     # candidates: subset of all possible indexes where to check condition for blank shape -> to save time
     indexes_candits_inside = get_indexes_canditate_inside_shape(point_center, radius)
 
-    # relative distance of candidate indexes to center
-    locs_rel2center_candits = indexes_candits_inside - point_center
+    # relative position of candidate indexes to center
+    points_rel2center_candits_inside = indexes_candits_inside - point_center
 
-    # condition for sphere: if distance to center is less than radius
-    is_indexes_inside = np.linalg.norm(locs_rel2center_candits, axis=3) <= radius
+    # distance to center -> compute the norm of relative position vectors
+    dist_rel2center_candits_inside = np.linalg.norm(points_rel2center_candits_inside, axis=3)
+
+    # condition for sphere: distance to center is less than radius
+    is_indexes_inside = np.abs(dist_rel2center_candits_inside) <= radius
     # array of ['True', 'False'], with 'True' for indexes that are inside the sphere
 
     indexes_inside = indexes_candits_inside[is_indexes_inside]
 
-    (indexes_x, indexes_y, indexes_z) = np.transpose(indexes_inside)
-    inout_image[indexes_z, indexes_y, indexes_x] = 1
+    (indexes_in_x, indexes_in_y, indexes_in_z) = np.transpose(indexes_inside)
+    inout_image[indexes_in_z, indexes_in_y, indexes_in_x] = 1
 
     return inout_image
 
@@ -52,7 +57,7 @@ def create_cylinder_mask(inout_image: np.ndarray,
                          point_center: Tuple[float, float, float] = None,
                          vector_axis: Tuple[float, float, float] = None,
                          radius_base: float = 50.0,
-                         length_axis: float = 50.0
+                         length_axis: float = 100.0
                          ) -> np.ndarray:
     if not point_center:
         point_center = (inout_image.shape[0] / 2,
@@ -63,62 +68,116 @@ def create_cylinder_mask(inout_image: np.ndarray,
         vector_axis = (0.0, 0.0, 1.0)
 
     norm_vector_axis = np.sqrt(np.dot(vector_axis, vector_axis))
-    unit_vector_axis = vector_axis / norm_vector_axis
+    unit_vector_axis = np.array(vector_axis) / norm_vector_axis
     half_length_axis = length_axis / 2.0
 
     # candidates: subset of all possible indexes where to check condition for blank shape -> to save time
-    dist_corner_2cen = np.sqrt(radius_base ** 2 + half_length_axis ** 2)
-    indexes_candits_inside = get_indexes_canditate_inside_shape(point_center, dist_corner_2cen)
+    dist_corner_2center = np.sqrt(radius_base ** 2 + half_length_axis ** 2)
+    indexes_candits_inside = get_indexes_canditate_inside_shape(point_center, dist_corner_2center)
 
-    # relative distance of candidate indexes to center
-    locs_rel2center_candits = indexes_candits_inside - point_center
+    # relative position of candidate indexes to center
+    points_rel2center_candits_inside = indexes_candits_inside - point_center
 
-    # conditions for cylinder: 1) if distance to center, parallel to axis, is less than length_axis
-    #                          2) if distance to center, perpendicular to axis, is less than radius_base
-    dist_rel2center_parall_axis_candits = np.abs(np.dot(locs_rel2center_candits, unit_vector_axis))
+    # distance to center, parallel to axis -> dot product of distance vectors with 'vector_axis'
+    dist_rel2center_parall_axis_candits = np.dot(points_rel2center_candits_inside, unit_vector_axis)
 
-    is_indexes_inside_parall = dist_rel2center_parall_axis_candits <= half_length_axis
-    is_indexes_inside_perpen = np.sqrt(np.square(np.linalg.norm(locs_rel2center_candits, axis=3))
-                                       - np.square(dist_rel2center_parall_axis_candits)) <= radius_base
+    # distance to center, perpendicular to axis -> Pythagoras theorem
+    dist_rel2center_perpen_axis_candits = np.sqrt(np.square(np.linalg.norm(points_rel2center_candits_inside, axis=3))
+                                                  - np.square(dist_rel2center_parall_axis_candits) + _EPS)
 
-    is_indexes_inside = np.logical_and(is_indexes_inside_parall, is_indexes_inside_perpen)
+    # conditions for cylinder: 1) distance to center, parallel to axis, is less than 'half_length_axis'
+    #                          2) distance to center, perpendicular to axis, is less than 'radius_base'
+    is_indexes_inside_cond1 = np.abs(dist_rel2center_parall_axis_candits) <= half_length_axis
+    is_indexes_inside_cond2 = np.abs(dist_rel2center_perpen_axis_candits) <= radius_base
+
+    is_indexes_inside = np.logical_and(is_indexes_inside_cond1, is_indexes_inside_cond2)
     # array of ['True', 'False'], with 'True' for indexes that are inside the cylinder
 
     indexes_inside = indexes_candits_inside[is_indexes_inside]
 
-    (indexes_x, indexes_y, indexes_z) = np.transpose(indexes_inside)
-    inout_image[indexes_z, indexes_y, indexes_x] = 1
+    (indexes_in_x, indexes_in_y, indexes_in_z) = np.transpose(indexes_inside)
+    inout_image[indexes_in_z, indexes_in_y, indexes_in_x] = 1
 
     return inout_image
 
 
-# def create_cuboid_in_image(inout_image: np.ndarray,
-#                            coords0: Tuple[int, int, int] = None,
-#                            halfsides: Tuple[int, int, int] = (50, 50, 50)
-#                            ) -> np.ndarray:
-#     if not coords0:
-#         coords0 = (int(inout_image.shape[0] / 2), int(inout_image.shape[1] / 2), int(inout_image.shape[2] / 2))
-#
-#     (z_min, z_max) = (coords0[0] - halfsides[0], coords0[0] + halfsides[0])
-#     (x_min, x_max) = (coords0[1] - halfsides[1], coords0[1] + halfsides[1])
-#     (y_min, y_max) = (coords0[2] - halfsides[2], coords0[2] + halfsides[2])
-#
-#     indexes_z_cuboid = []
-#     indexes_x_cuboid = []
-#     indexes_y_cuboid = []
-#     for iz in range(z_min, z_max, 1):
-#         for ix in range(x_min, x_max, 1):
-#             for iy in range(y_min, y_max, 1):
-#                 indexes_z_cuboid.append(iz)
-#                 indexes_x_cuboid.append(ix)
-#                 indexes_y_cuboid.append(iy)
-#             # endfor
-#         # endfor
-#     # endfor
-#     indexes_cuboid = [indexes_z_cuboid, indexes_y_cuboid, indexes_y_cuboid]
-#
-#     inout_image[indexes_cuboid] = 1
-#     return inout_image
+def create_cuboid_mask(inout_image: np.ndarray,
+                       point_center: Tuple[float, float, float] = None,
+                       vector_axis: Tuple[float, float, float] = None,
+                       vector_seg1_base: Tuple[float, float, float] = None,
+                       len_seg_base: float = 100.0,
+                       length_axis: float = 100.0
+                       ) -> np.ndarray:
+    if not point_center:
+        point_center = (inout_image.shape[0] / 2,
+                        inout_image.shape[1] / 2,
+                        inout_image.shape[2] / 2)
+
+    if not vector_axis:
+        vector_axis = (0.0, 0.0, 1.0)
+
+    if not vector_seg1_base:
+        # try perpendicular vectors to 'vector_axis'
+        vector_perpen_2axis_1 = (0.0, -vector_axis[2], vector_axis[1])
+        vector_perpen_2axis_2 = (-vector_axis[2], 0.0, vector_axis[0])
+        vector_perpen_2axis_3 = (-vector_axis[1], vector_axis[0], 0.0)
+        vectors_perpen_2axis_try = [vector_perpen_2axis_1, vector_perpen_2axis_2, vector_perpen_2axis_3]
+
+        for vector_perpen_2axis in vectors_perpen_2axis_try:
+            # check the perpendicular vector is null
+            if np.all(np.abs(vector_perpen_2axis) < _EPS):
+                continue
+            else:
+                vector_seg1_base = vector_perpen_2axis
+                break
+
+    norm_vector_axis = np.sqrt(np.dot(vector_axis, vector_axis))
+    unit_vector_axis = np.array(vector_axis) / norm_vector_axis
+    norm_vector_seg1_base = np.sqrt(np.dot(vector_seg1_base, vector_seg1_base))
+    unit_vector_seg1_base = np.array(vector_seg1_base) / norm_vector_seg1_base
+    half_length_axis = length_axis / 2.0
+    half_len_seg_base = len_seg_base / 2.0
+
+    # candidates: subset of all possible indexes where to check condition for blank shape -> to save time
+    dist_corner_2center = np.sqrt(2 * half_len_seg_base ** 2 + half_length_axis ** 2)
+    indexes_candits_inside = get_indexes_canditate_inside_shape(point_center, dist_corner_2center)
+
+    # relative position of candidate indexes to center
+    points_rel2center_candits_inside = indexes_candits_inside - point_center
+
+    # distance to center, parallel to axis -> dot product of distance vectors with 'vector_axis'
+    dist_rel2center_parall_axis_candits = np.dot(points_rel2center_candits_inside, unit_vector_axis)
+
+    # candidate distance vectors (component) perpendicular to 'vector_axis'
+    points_rel2center_perpen_axis_candits = \
+        points_rel2center_candits_inside - dist_rel2center_parall_axis_candits[..., np.newaxis] * unit_vector_axis
+
+    # distance to center, perpen. to axis, parall. to seg1 -> dot prod. perpen. distance vectors with 'vector_seg1_base'
+    dist_rel2center_perpen_axis_parall_seg1_candits = \
+        np.dot(points_rel2center_perpen_axis_candits, unit_vector_seg1_base)
+
+    # distance to center, perpendicular to axis and seg1 -> Pythagoras theorem
+    dist_rel2center_perpen_axis_perpen_seg1_candits = \
+        np.sqrt(np.square(np.linalg.norm(points_rel2center_perpen_axis_candits, axis=3))
+                - np.square(dist_rel2center_perpen_axis_parall_seg1_candits) + _EPS)
+
+    # conditions cuboid: 1) dist. to center, parallel to axis: is less than 'half_length_axis'
+    #                    2) dist. to center, perpen. to axis, parall. to seg1, is less than 'half_len_seg_base'
+    #                    3) dist. to center, perpendicular to axis and seg1, is less than 'half_len_seg_base'
+    is_indexes_inside_cond1 = np.abs(dist_rel2center_parall_axis_candits) <= half_length_axis
+    is_indexes_inside_cond2 = np.abs(dist_rel2center_perpen_axis_parall_seg1_candits) <= half_len_seg_base
+    is_indexes_inside_cond3 = np.abs(dist_rel2center_perpen_axis_perpen_seg1_candits) <= half_len_seg_base
+
+    is_indexes_inside = \
+        np.logical_and(is_indexes_inside_cond1, np.logical_and(is_indexes_inside_cond2, is_indexes_inside_cond3))
+    # array of ['True', 'False'], with 'True' for indexes that are inside the cylinder
+
+    indexes_inside = indexes_candits_inside[is_indexes_inside]
+
+    (indexes_in_x, indexes_in_y, indexes_in_z) = np.transpose(indexes_inside)
+    inout_image[indexes_in_z, indexes_in_y, indexes_in_x] = 1
+
+    return inout_image
 
 
 def main(args):
@@ -126,10 +185,10 @@ def main(args):
     # Create image
     out_shape_image = (256, 256, 256)
     out_image = np.zeros(out_shape_image)
-    out_image = create_cylinder_mask(out_image,
-                                     vector_axis=(1.0,1.0,1.0),
-                                     radius_base=50.0,
-                                     length_axis=50.0)
+    out_image = create_cuboid_mask(out_image,
+                                   vector_axis=(1.0, 1.0, 1.0),
+                                   len_seg_base=100.0,
+                                   length_axis=100.0)
 
     # write out image
     print("Write out image: %s..." % (args.output_file))
